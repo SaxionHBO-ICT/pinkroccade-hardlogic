c9fcc30372a6c6ff9e04558d42de58e86787c3d5
vwierenga <vwierenga@gmail.com>
2016-06-08 11:41:42 +0200

    Added XML test code

--- app/src/main/java/com/logic/hard/projecthardlogic/activity/MainActivity.java
+++ app/src/main/java/com/logic/hard/projecthardlogic/activity/MainActivity.java
@@ -1,9 +1,21 @@
 package com.logic.hard.projecthardlogic.activity;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.logic.hard.projecthardlogic.R;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +23,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("xmlTest", "onCreate");
+
+        XmlPullParserFactory pullParserFactory;
+        try {
+            pullParserFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = pullParserFactory.newPullParser();
+
+            InputStream in_s = getApplicationContext().getAssets().open("test.xml");
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in_s, null);
+
+            parseXML(parser);
+
+        } catch (XmlPullParserException e) {
+            Log.v("xmlTest", "XmlPullParserException");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.v("xmlTest", "IOException");
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -18,4 +52,40 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().popBackStackImmediate();
         super.onResume();
     }
+
+    private void parseXML(XmlPullParser parser) throws XmlPullParserException,IOException
+    {
+        int eventType = parser.getEventType();
+        Log.v("xmlTest", "begin");
+
+        while (eventType != XmlPullParser.END_DOCUMENT){
+            String name = null;
+            switch (eventType){
+                case XmlPullParser.START_DOCUMENT:
+                    //products = new ArrayList();
+                    break;
+                case XmlPullParser.START_TAG:
+                    name = parser.getName();
+                    if (name.equals("product")){
+                        Log.v("xmlTest", "product");
+                    } else {
+                        if (name.equals("productname")){
+                            Log.v("xmlTest", parser.nextText());
+                        } else if (name.equals("productcolor")){
+                            Log.v("xmlTest", parser.nextText());
+                        } else if (name.equals("productquantity")){
+                            Log.v("xmlTest", parser.nextText());
+                        }
+                    }
+                    break;
+                case XmlPullParser.END_TAG:
+                    name = parser.getName();
+                    //if (name.equalsIgnoreCase("product") && currentProduct != null){
+                    //    products.add(currentProduct);
+                    //}
+            }
+            eventType = parser.next();
+        }
+    }
+
 }
